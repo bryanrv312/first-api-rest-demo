1f872d6beec813cf3408b2b64d02aa7723aeec0d
bryam <bryamrubio312@gmail.com>
2025-05-16 12:59:43 -0500

    actualizacion controller

--- first-api-rest/src/main/java/com/first/api/controller/HelloController.java
+++ first-api-rest/src/main/java/com/first/api/controller/HelloController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
-    //Este es un nuevo comentario
-    //Agregamos algo nuevo 2
+    //comentario bryangi
     @GetMapping("/hello")
     public String saludar(){
         return "Hola Mundo con REST en Spring Boot 3";
